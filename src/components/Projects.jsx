cf91895a51171659014aa69253862f4db850a532
KcastroT <a01798925@tec.mx>
2025-05-12 13:13:16 -0600

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -46,7 +46,7 @@ const Projects = () => {
   return (
     <div className="px-4 py-12 bg-gray-300" id="projects">
       <h2 className="text-3xl font-bold text-center mb-10">
-        Project / Experience
+        Projects / Experience
       </h2>
       <div className="grid gap-8 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-stretch">
         {ProjectsData.map((project, index) => (
